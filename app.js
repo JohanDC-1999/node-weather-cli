c370059bb1c4f3fe4f8582bd667d5e4838bec3db
JohanDC-1999 <johan.00444@gmail.com>
2025-03-24 14:36:44 +0200

    Added a loader animation

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
+const { write } = require("fs");
+
 require("dotenv").config();
 
 const API_KEY = process.env.WEATHER_API_KEY;
 
+const LOADER = ['|', '/', '-', '\\'];
+
+function showLoader(ms = 100) {
+  let i = 0;
+  return setInterval(() => {
+    process.stdout.write(`\r${LOADER[i % LOADER.length]} \x1b[1;31mWorking hard...`);
+    i++;
+  }, 100);
+}
+
+/**
+ * Main function used the the current weather information
+ * @param {string} location 
+ * @param {string} units 
+ */
 async function getCurrentWeather(location, units = 'celsius') {
   const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${location}&aqi=no`;
+  const loader = showLoader(); // Start the loader
+
   try {
+    // Make a request to the api
     const response = await fetch(url);
     if (!response.ok) {
       // Parse the JSON error response
       const errorData = await response.json();
+          process.stdout.write('\r'); // Clear the loader line
       console.log(JSON.stringify(errorData)); // Log the error JSON
       throw new Error(
         `Response status: ${response.status}, Error: ${errorData.error.message}`
@@ -25,10 +46,11 @@ async function getCurrentWeather(location, units = 'celsius') {
     } else if (lowerCaseUnits === 'celsius' || lowerCaseUnits === 'c') {
       temperature = `${data.current.temp_c}°C`;
     } else {
-      console.log("Invalid units provided. Defaulting to Celsius.")
+      console.log("\rInvalid units provided. Defaulting to Celsius.")
       temperature = `${data.current.temp_c}°C`;
     }
 
+    // Store the data in an object
     const weatherData = {
       city: data.location.name,
       region: data.location.region,
@@ -37,12 +59,17 @@ async function getCurrentWeather(location, units = 'celsius') {
       condition: data.current.condition.text,
     };
 
-    console.log(`
+    clearInterval(loader); // Stop the loader
+  
+    // Log the weather data to the user
+    process.stdout.write(`\rHere is the weather information for ${weatherData.city}
       Weather in ${weatherData.city}, ${weatherData.region}, ${weatherData.country}:
       Temperature: ${weatherData.temperature}
       Condition: ${weatherData.condition}
       `);
   } catch (error) {
+    clearInterval(loader);
+    process.stdout.write('\r');
     console.error(error.message);
   }
 }
